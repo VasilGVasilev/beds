6324ff91b068faa48a028f3fcb2ecbb6b59a6387
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-08-14 22:40:44 +0300

    order conceptually done

--- beds/app/order/Order.js
+++ beds/app/order/Order.js
@@ -7,136 +7,115 @@ import { motion } from "framer-motion";
 
 export const revalidate = 0; // revalidate this page every 60 seconds
 
-const buildingProcessLayout ="pb-10 px-5 h-full w-full lg:h-96 flex justify-center"
+const buildingProcessLayout = "pb-10 px-5 h-full w-full lg:h-96 flex justify-center"
 export default function Order() {
 
     return (
         <>
 
-            {/* DESCRIPTION */}
-            <motion.div
-                className="bg-gradient-ixora"
-                initial={{ opacity: 0, width: 0 }}
-                animate={{ opacity: 1, width: '100%' }}
-                transition={{ delay: 0.3, duration: 1 }}
+            {/* Container for Steps */}
+            <div className="bg-ixora-dark px-5 w-full h-full flex-col justify-center items-center space-y-5">
+                {/* Container for image with overflow effect, this template to be used for subsequent pictures */}
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    className="flex justify-center items-center"
+                    viewport={{ once: true }}
+                    transition={{ delay: 0.4, duration: 0.6 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -200 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
                 >
-                    <div className="text-white py-4 text-center text-3xl font-bold truncate">За сградата</div>
-            </motion.div>
-
-
-            {/* TIMELINE */}
-            <motion.div
-                className="bg-gradient-ixora py-4"
-                initial={{ opacity: 0, width: 0 }}
-                animate={{ opacity: 1, width: '100%' }}
-                transition={{ delay: 0.3, duration: 1 }}
+                    <div>1</div>
+                    <div>
+                        asdasd
+                    </div>
+                </motion.div>
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    className="flex justify-center items-center"
+                    viewport={{ once: true }}
+                    transition={{ delay: 0.4, duration: 0.6 }}
+                    variants={{
+                        hidden: { opacity: 0, x: 200 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
                 >
-                    <div className="text-white text-center text-3xl font-bold">Етапи</div>
-            </motion.div>
-
-
-            {/* BUILDING PROGRESS */}
-            <div>
-                <motion.div
-                    className="bg-gradient-ixora py-4"
-                    initial={{ opacity: 0, width: 0 }}
-                    animate={{ opacity: 1, width: '100%' }}
-                    transition={{ delay: 0.6, duration: 1 }}
-                    >
-                        <div className="text-white text-center text-2xl font-bold">Етап на проекта - Акт 14</div>
+                    <div>2</div>
+                    <div>
+                        asdasd
+                    </div>
                 </motion.div>
-                <div className="p-10 bg-ixora-dark flex flex-col lg:flex-row">
-                    <motion.div
-                        className={buildingProcessLayout}
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{ once: true, amount: 0.5 }}
-                        transition={{ delay: 0.1, duration: 0.5 }}
-                        variants={{
-                            hidden: { opacity: 0, x: 50},
-                            visible: { opacity: 1, x: 0},
-                        }}
-                    >
-                        <Image
-                            loader={imageLoader}
-                            src="/buildProcessOne.webp"
-                            alt="Build Process"
-                            width={1310}
-                            height={887}
-                        >
-                        </Image>
-                    </motion.div>
-                    <motion.div
-                        className={buildingProcessLayout}
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{ once: true, amount: 0.5 }}
-                        transition={{ delay: 0.3, duration: 0.5 }}
-                        variants={{
-                            hidden: { opacity: 0, x: 50},
-                            visible: { opacity: 1, x: 0},
-                        }}
-                    >
-                        <Image
-                            loader={imageLoader}
-                            src="/buildProcessTwo.webp"
-                            alt="Build Process"
-                            width={1310}
-                            height={887}
-                        >
-                        </Image>
-                    </motion.div>
-                    <motion.div
-                        className={buildingProcessLayout}
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{ once: true, amount: 0.5 }}
-                        transition={{ delay: 0.5, duration: 0.5 }}
-                        variants={{
-                            hidden: { opacity: 0, x: 50},
-                            visible: { opacity: 1, x: 0},
-                        }}
-                    >
-                        <Image
-                            loader={imageLoader}
-                            src="/buildProcessThree.webp"
-                            alt="Build Process"
-                            width={1310}
-                            height={887}
-                        >
-                        </Image>
-                    </motion.div>
-                </div>
-            </div>
-
-            {/* AKT 16 */}
-            <>
                 <motion.div
-                    className="bg-gradient-ixora py-4"
-                    initial={{ opacity: 0, width: 0 }}
-                    animate={{ opacity: 1, width: '100%' }}
-                    transition={{ delay: 0.9, duration: 1 }}
-                    >
-                    <div className="text-white text-center text-2xl font-bold">Изберете Вашия бъдещ дом</div>
+                    initial="hidden"
+                    whileInView="visible"
+                    className="flex justify-center items-center"
+                    viewport={{ once: true }}
+                    transition={{ delay: 0.4, duration: 0.6 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -200 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <div>3</div>
+                    <div>
+                        asdasd
+                    </div>
                 </motion.div>
-                <div className="bg-ixora-img-description h-60 bg-center bg-cover bg-no-repeat bg-white md:min-h-[32rem] md:grid md:place-items-center md:bg-fixed md:bg-no-repeat md:bg-cover md:bg-center md:opacity-75">
-                </div>
-            </>
-
-            {/* LOCATION*/}
-            <div>
-
                 <motion.div
-                    className="bg-gradient-ixora py-4"
-                    initial={{ opacity: 0, width: 0 }}
-                    animate={{ opacity: 1, width: '100%' }}
-                    transition={{ delay: 0.3, duration: 1 }}
-                    >
-                    <div className="text-white text-center text-2xl font-bold">Местоположение</div>
+                    initial="hidden"
+                    whileInView="visible"
+                    className="flex justify-center items-center"
+                    viewport={{ once: true }}
+                    transition={{ delay: 0.4, duration: 0.6 }}
+                    variants={{
+                        hidden: { opacity: 0, x: 200 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <div>4</div>
+                    <div>
+                        asdasd
+                    </div>
                 </motion.div>
-                <Maps></Maps>
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    className="flex justify-center items-center"
+                    viewport={{ once: true }}
+                    transition={{ delay: 0.4, duration: 0.6 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -200 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <div>5</div>
+                    <div>
+                        asdasd
+                    </div>
+                </motion.div>
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    className="flex justify-center items-center"
+                    viewport={{ once: true }}
+                    transition={{ delay: 0.4, duration: 0.6 }}
+                    variants={{
+                        hidden: { opacity: 0, x: 200 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <div>6</div>
+                    <div>
+                        asdasd
+                    </div>
+                </motion.div>
+
             </div>
 
         </>
     )
 }
+
